7859c2be14d5e14dbdbfb0679182f087979cc7ad
ghanem025 <ghanemxghanem@gmail.com>
2022-10-04 19:31:40 -0400

    working on luhn algorithm logic

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,27 @@ int main(){
 	string card_num;
 	int total;
 	cout << "Enter your card number";
-	cin >> card_num;
+	cin >> card_num; // enter card number
 	cout << card_num << '\n';
-	for (int i=0;i<card_num.size();i++){
-		if (i % 2 != 0){
+	for (int i=0;i<card_num.size();i++){ // look through each number
+		
+		if (i % 2 != 0){ // if the number is not even add it to the total
 			cout << card_num[i];
 			int total = card_num[i] * 2;
+			if (total >= 10){
+				int num1 = total % 10;
+				int num2 = total / 10
+				cout << num1;
+				cout << num2; 
+				
+			}
 			cout << total;
 			cout << '\n';
+		}
+		else if (i%2 == 0){
+			cout << card_num[i]
+			int total = card_num[i] * 1;
+			
 		}
 	}
 	cout << "Testing out";
